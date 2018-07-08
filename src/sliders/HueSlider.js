d2c0afb2cb28d3696132159f3ad18d32d7465167
jtweaver <jtweaver22@gmail.com>
2018-07-08 15:24:37 -0500

    On sliding complete

--- src/sliders/HueSlider.js
+++ src/sliders/HueSlider.js
@@ -5,7 +5,7 @@ import GradientSlider from './GradientSlider';
 import HueGradient from '../gradients/HueGradient';
 import tinycolor from 'tinycolor2';
 
-const HueSlider = ({ style, value, onValueChange, gradientSteps, trackClickable }) => {
+const HueSlider = ({ style, value, onValueChange, gradientSteps, trackClickable, onSlidingComplete }) => {
   return (
     <GradientSlider
       gradient={<HueGradient gradientSteps={gradientSteps} />}
@@ -16,6 +16,7 @@ const HueSlider = ({ style, value, onValueChange, gradientSteps, trackClickable 
       thumbTintColor={tinycolor({ s: 1, l: 0.5, h: value }).toHslString()}
       onValueChange={onValueChange}
       trackClickable={trackClickable}
+      onSlidingComplete={onSlidingComplete}
     />
   );
 };
